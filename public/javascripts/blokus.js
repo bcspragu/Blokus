652cb24385a19268fc74347fae8765d9ddf74b1d
Brandon Sprague <brandon00sprague@gmail.com>
2013-12-23 00:01:06 -0500

    Working on it
    Added movable pieces, need to add check for validity

--- public/javascripts/blokus.js
+++ public/javascripts/blokus.js
@@ -1,6 +1,10 @@
 var paper;
 var block_size = 25;
 
+var avail_pieces = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20];
+var current_piece;
+var current_piece_index = 20;
+
 //The pieces are stored as an array of x,y coordinates
 var pieces = [
   [], //One piece
@@ -30,16 +34,29 @@ $(function(){
   var socket = io.connect('http://localhost');
 
   paper =  Raphael("board", block_size*20, block_size*20+200);
+  
+  //Draw the grid
   for(var i = 0; i < 20; i++){
     for(var j = 0; j < 20; j++){
       paper.rect(i*block_size,j*block_size,block_size,block_size).attr({stroke: '#000', 'stroke-width':1});
     }
   }
   var index = 0;
-  for(var i = 1; i < 18; i += 5){
-    for(var j = 1; j < 18; j += 5){
-      new Piece(pieces[index++],i,j,paper);
-    }
-  }
+  current_piece = new Piece(pieces[current_piece_index],10,25,paper);
   paper.rect(0,20*block_size,block_size*20,200).attr({fill: '#f99'})
+
+  var select_piece = function(direction){
+    current_piece.deletePiece();
+    current_piece_index += direction;
+    current_piece = new Piece(pieces[current_piece_index % 21],9,23,paper);
+  }
+
+  paper.path('M50,550L0,600L50,650L50,550').attr({fill: '#000'}).click(function(){
+    select_piece(-1);
+  });
+
+  paper.path('M450,550L500,600L450,650L450,550').attr({fill: '#000'}).click(function(){
+    select_piece(1);
+  });
+
 });
--- public/javascripts/piece.js
+++ public/javascripts/piece.js
@@ -4,14 +4,33 @@ function Piece(piece_array,x,y,paper) {
   var p = this;
   p.blocks = [];
   p.selected = false;
+  p.mousemove = function(e){
+    var board = $('#board');
+    if(p.selected){
+      x = Math.floor((e.pageX - board.offset().left)/block_size);
+      y = Math.floor((e.pageY - board.offset().top)/block_size);
+      p.moveAbsolute([x,y]);
+    }
+  }
 
+  $('#board').mousemove(p.mousemove);
   p.drawBlock = function(coordinates){
     var block = paper.rect((x+coordinates[0])*block_size,(y+coordinates[1])*block_size,block_size,block_size).attr({fill: '#0f0'});
     block.click(function(){
+      //If valid, drop it there, otherwise, send it back
       if(p.selected){
-        block.attr({fill: '#f00'});
+        //Color it back
+        for(var i = 0; i < p.blocks.length; i++){
+          p.blocks[i].attr({fill: '#0f0'});
+        }
+        //If valid, leave it there, otherwise put it back
+        if(true){
+          p.moveAbsolute([9,23]);
+        }
       }else{
-
+        for(var i = 0; i < p.blocks.length; i++){
+          p.blocks[i].attr({fill: '#f00'});
+        }
       }
       p.selected = !p.selected;
     });
@@ -19,21 +38,29 @@ function Piece(piece_array,x,y,paper) {
   }
 
   p.moveRelative = function(coordinates){
-    for(var block in p.blocks){
-      var x = block.attr('x');
-      var y = block.attr('y');
-      block.attr({x: x+coordinates[0]*block_size, y: y+coordinates[1]*block_size});
+    for(var i = 0; i < p.blocks.length; i++){
+      var x = p.blocks[i].attr('x');
+      var y = p.blocks[i].attr('y');
+      p.blocks[i].attr({x: x+coordinates[0]*block_size, y: y+coordinates[1]*block_size});
     }
   }
 
   p.moveAbsolute = function(coordinates){
-    for(var block in p.blocks){
-      blocks.attr({x: coordinates[0]*block_size, y: coordinates[1]*block_size});
+    p.blocks[0].attr({x: coordinates[0]*block_size, y: coordinates[1]*block_size});
+    for(var i = 1; i < p.blocks.length; i++){
+      p.blocks[i].attr({x: (coordinates[0]+piece_array[i-1][0])*block_size, y: (coordinates[1]+piece_array[i-1][1])*block_size});
     }
   }
 
   p.drawBlock([0,0]);
   for(var i = 0; i < piece_array.length; i++){
     p.drawBlock(piece_array[i]);
+  }
+
+  p.deletePiece = function(){
+    for(var i = 0; i < p.blocks.length; i++){
+        p.blocks[i].remove();
+      }
+    $('#board').off('mousemove');
   }
 }
