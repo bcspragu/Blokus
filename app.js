7beeb2e684bdf9ff631abb9b50396f863ffba94d
Brandon Sprague <brandon00sprague@gmail.com>
2014-01-09 00:04:24 -0500

    Serverside game structure

--- app.js
+++ app.js
@@ -11,6 +11,8 @@ var path = require('path');
 var app = express();
 var server = http.createServer(app);
 var io = require('socket.io').listen(server);
+var id = 0;
+var games = [{players: [], current_turn: 0, board: []}];
 
 // all environments
 app.set('port', process.env.PORT || 3000);
@@ -32,6 +34,18 @@ if ('development' == app.get('env')) {
 }
 
 app.get('/', routes.index);
+
+io.sockets.on('connection', function (socket) {
+  var user_id = id++;
+  socket.set('id', user_id,function(){
+    //If the last game is full, start a new one
+    if(games[games.length - 1].players.length == 4){
+      game.push({players: [user_id], current_turn: 0, board: []})
+    }else{
+      games[games.length - 1].players.push(user_id);
+    }
+  });
+});
 
 server.listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
